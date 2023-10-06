3f675fd0183ea8a873635f012bf24efaae3ba000
Student <Student@CLB3Unit33>
2023-10-06 15:45:03 +0800

    Version1

---
+++ src/baltazar_quiz3/Baltazar_Quiz3.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package baltazar_quiz3;
+
+/**
+ *
+ * @author Student
+ */
+public class Baltazar_Quiz3 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
